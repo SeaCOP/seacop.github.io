80136982982921cbbdd1d528aa7461ddeba66984
Josh Dukes <hex@neg9.org>
2021-12-17 21:26:37 -0800

    added allegations from data.seattle.gov

--- data_munger.py
+++ data_munger.py
@@ -33,8 +33,6 @@ logger = logging.getLogger(__name__)
 complaints_url = "https://data.seattle.gov/api/views/99yi-dthu/rows.csv"
 use_of_force_url = "https://data.seattle.gov/api/views/ppi5-g2bj/rows.csv"
 
-
-
 def normalize_fieldnames(row):
     if "Name" in row:
         row.append("OrigName")
@@ -61,19 +59,68 @@ def item_list_to_keyed_dict(item_list, key):
     return {item[key]: item for item in item_list}
 
 
-# logger.info("Downloading complaints dataset...")
-# complaints = pd.read_csv(complaints_url)
+# 2020OPA-0203
 
-# logger.info("Downloading use of force data set...")
-# use_of_force = pd.read_csv(use_of_force_url)
+# allegations
+# OPA # = 'OPA #' == complaints["File Number"]
+# Occurred date = 'Occurred Date' 
+# Incident type = 'Incident Type'
+# Allegation = 'Allegation'
+# Finding = 'Finding'
+# Finding date = 'Investigation End Date'
+# Action Taken = 'Action taken' == complaints["Discipline"]
+
+# "ID #" = 'Named Employee ID'
+
+
+logger.info("Downloading complaints dataset...")
+complaints = pd.read_csv(complaints_url)
 
 allegations = []
 p = '_data/allegations'
 for f in os.listdir(p):
     with open(os.path.join(p,f)) as fd:
-        allegations_fieldnames=normalize_fieldnames(next(reader(fd)))
+        allegations_fieldnames = normalize_fieldnames(next(reader(fd)))
         dr = DictReader(fd, fieldnames=allegations_fieldnames)
-        allegations.extend(row for row in dr)
+        # removing 2015OPA-1919 which apparently isn't to anyone
+        allegations.extend(row for row in dr if 'ID #' in row)
+
+with open("_data/named_employee_id_map.csv") as fd:
+    named_employee_id_map_fieldnames = [i.strip() for i in next(reader(fd))]
+    dr = DictReader(fd, fieldnames=named_employee_id_map_fieldnames)
+    named_employee_map = {r['Named Employee ID']:r["ID #"] for r in dr}
+        
+# make sure they're the same
+allegation_names = set(f"{row['Last name']},{row['First name']}"
+		       for row in allegations)
+
+missing_officer_ids = []
+allegations_from_complaints = []
+officer_allegations = {(a['ID #'],a["OPA #"]) for a in allegations}
+for _, complaint in complaints.iterrows():
+    if not complaint["Case Status"] == 'Completed':
+        continue
+    try:
+        officer_badge_number = named_employee_map[str(complaint["Named Employee ID"])]
+    except KeyError:
+        missing_officer_ids.append(complaint["Named Employee ID"])
+        continue
+    opa_file = complaint["File Number"]
+    if not (officer_badge_number, opa_file) in officer_allegations:
+        allegations_from_complaints.append(
+            {'OPA #': opa_file, 
+             'ID #': officer_badge_number,
+             'Occurred date': complaint['Occurred Date'],
+             'Incident type': complaint['Incident Type'],
+             'Allegation': complaint['Allegation'],
+             'Finding': complaint['Finding'],
+             'Action taken':complaint["Discipline"]})
+        officer_allegations.add((officer_badge_number, opa_file))
+allegations.extend(allegations_from_complaints)
+
+logger.info("Downloading use of force data set...")
+use_of_force = pd.read_csv(use_of_force_url)
+
 
 with open("_data/rosters/2020.05.08.csv") as fd:
     fd.seek(3) # Skip BOM
@@ -86,31 +133,15 @@ with open("_data/compensation/2019.csv") as fd:
     dr = DictReader(fd, fieldnames=compensation_fieldnames)
     compensation = [normalize_fields(row) for row in dr]
 
-missing_officers = []
-with open("_data/named_employee_id_map.csv") as fd:
-    named_employee_id_map_fieldnames = [i.strip() for i in next(reader(fd))]
-    for dr in DictReader(fd, fieldnames=named_employee_id_map_fieldnames):
-        try:
-            m = next(o for o in roster if dr["ID #"] == o["Badge_Num"])
-            m["Anonymous_ID"] = dr['Named Employee ID']
-        except StopIteration:
-            missing_officers.append(dr)
-print(f"Missing {len(missing_officers)} from roster")
-
-
-# make sure they're the same
-# next(i for i in compensation if roster[0]["Name"] ==  i["Name"])
-allegation_names = set(f"{row['Last name']},{row['First name']}"
-		       for row in allegations)
 
 compensation_names = set(r["Name"] for r in compensation)
 roster_names = set(r["Name"] for r in roster)
 # roster_names.difference(compensation_names)
 compensation_gap_size = len(roster_names.difference(compensation_names))
 if compensation_gap_size:
-    print(f"there are {compensation_gap_size} unmatched compensation records")
+    logger.info(f"there are {compensation_gap_size} unmatched compensation records")
     matched_percent = 100-((compensation_gap_size*100//len(roster_names)))
-    print(f"{matched_percent}% matched")
+    logger.info(f"{matched_percent}% matched")
 
 # make sure we haven't lost any rows through normalization
 roster_duplicate_count = len(roster)-len(roster_names)
@@ -118,14 +149,14 @@ if roster_duplicate_count:
     full_list = [r["Name"] for r in roster]
     roster_duplicates = [name for name in roster_names
                          if full_list.count(name) > 1]
-    print(f"found {roster_duplicate_count} roster duplicates")
+    logger.info(f"found {roster_duplicate_count} roster duplicates")
 
 compensation_duplicate_count = len(compensation)-len(compensation_names)
 if compensation_duplicate_count:
     full_list = [r["Name"] for r in compensation]
     compensation_duplicates = [name for name in compensation_names
                          if full_list.count(name) > 1]
-    print(f"found {compensation_duplicate_count} compensation duplicates")
+    logger.info(f"found {compensation_duplicate_count} compensation duplicates")
 
 if roster_duplicate_count and compensation_duplicate_count:
     set(compensation_duplicates).intersection(set(roster_duplicates))
@@ -153,9 +184,7 @@ for i in ["allegations", "compensation"]:
 
 # create all .md files
 # Badge_Num
-
 # _officers/
-
 existing_officer_pages = [i.strip('.md')
                           for i in os.listdir('_officers/')]
 badge_numbers = [i["Badge_Num"] for i in roster]
